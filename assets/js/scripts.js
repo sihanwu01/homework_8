df5017beff477cf3ebc64d4fbe2d77673eece84f
unknown <sihanwu@andrew.cmu.edu>
2021-05-07 18:04:09 -0400

    images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -53,3 +53,65 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery);
+
+// Text Animation
+var animation = {};
+animation.opacityIn = [0,1];
+animation.scaleIn = [0.2, 1];
+animation.scaleOut = 3;
+animation.durationIn = 800;
+animation.durationOut = 600;
+animation.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.animation .letters-1',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.animation .letters-1',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  }).add({
+    targets: '.animation .letters-2',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.animation .letters-2',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  }).add({
+    targets: '.animation .letters-3',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.animation .letters-3',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  }).add({
+    targets: '.animation',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
