6adefc0ab4e25912fc86107fa41030edff1382d8
unknown <sihanwu@andrew.cmu.edu>
2021-05-07 11:55:37 -0400

    edited home page

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using anime.js
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+      if (
+          location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+      ) {
+          var target = $(this.hash);
+          target = target.length ?
+              target :
+              $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+              anime({
+                  targets: 'html, body',
+                  scrollTop: target.offset().top - 72,
+                  duration: 1000,
+                  easing: 'easeInOutExpo'
+              });
+              return false;
+          }
+      }
+  });
+
+})(jQuery);
