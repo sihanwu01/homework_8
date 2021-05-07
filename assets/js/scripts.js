f6d53e27a3f007f87c12c48043a41ff7f47ff1c4
unknown <sihanwu@andrew.cmu.edu>
2021-05-07 13:39:49 -0400

    nav bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,4 +24,32 @@
       }
   });
 
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+    } else {
+        $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+      if ($("#mainNav").offset().top > 100) {
+          $("#mainNav").addClass("navbar-shrink");
+      } else {
+          $("#mainNav").removeClass("navbar-shrink");
+      }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
 })(jQuery);
